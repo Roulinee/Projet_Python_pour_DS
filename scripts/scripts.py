bae098b94d026925a68b1de2e850052b8d91e0d5
rouline <juline.ley@ensae.fr>
2025-12-08 12:35:20 +0000

    Modifs conversion xlsx + importation données

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -3,11 +3,15 @@ import pandas as pd
 
 np.random.seed(123)
 
-df = pd.read_excel("/home/onyxia/work/Projet_Python_pour_DS/data/hadopi-2020-musique-data.ods", engine="odf")
+df = pd.read_excel("/home/onyxia/work/Projet_Python_pour_DS/data/hadopi-2020-musique-data.xlsx")
 obs = df.shape[0]
 print(obs)
 
-df_recode = df.assign(sexe = df["QSEXE"].map({"Femme": 0, "Homme": 1}))
-df_recode = df.assign(tranche_age = df["RAGE2"].map({"De15a24ans": 1, "De25a34ans1": 2, "De35a49ans1": 3, "De50a64ans1": 4, "De65aPlus": 5}))
+df_recode = df.assign(
+    sexe = df["QSEXE"].map({"FEMME": 0, "HOMME": 1}),
+    tranche_age = df["RAGE2"].map({"De15a24ans": 1, "De25a34ans1": 2, "De35a49ans1": 3, "De50a64ans1": 4, "De65aPlus": 5})
+)
 
-print(df_recode[["sexe"]])
+print(df_recode["sexe"])
+
+print(df_recode.describe())
