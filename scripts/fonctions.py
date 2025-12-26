18a9793e1fc7982333a5dc8d20feba848b92e6a4
rouline <juline.ley@ensae.fr>
2025-12-26 18:25:07 +0000

    fonction analyse + maj requirements

--- scripts/fonctions.py
+++ scripts/fonctions.py
@@ -1,12 +1,38 @@
+import requests
+import pandas as pd
+
 def telecharger(url, nom):
-    """"Importe le fichier de données sur le site
+    """"
+    Importe le fichier de données sur le site
     url (str) : url du fichier que l'on souhaite télécharger
-    nom (str) : nom du fichier qui apparaîtra  """
+    nom (str) : nom du fichier qui apparaîtra  
+    """
     response = requests.get(url)
     if response.status_code == 200:
-    with open(nom, "wb") as file:
-        file.write(response.content)
-    print("Téléchargement réussi !")
-else:
-    print("Erreur :", response.status_code)
+        with open(nom, "wb") as file:
+            file.write(response.content)
+        print("Téléchargement réussi !")
+    else:
+        print("Erreur :", response.status_code)
 
+def analyse(df):
+    n_obs, n_var = df.shape
+
+    n_num = df.select_dtypes(include="number").shape[1]
+    n_txt = df.select_dtypes(include="object").shape[1]
+
+    n_missing = df.isna().sum().sum()
+    pct_missing = round(100 * n_missing / (n_obs * n_var), 2)
+
+    n_var_missing = (df.isna().sum() > 0).sum()
+    n_const = (df.nunique(dropna=False) <= 1).sum()
+
+    phrase = (
+        f"La base de données {df} contient {n_obs} observations et {n_var} variables. "
+        f"Elle comprend {n_num} variables numériques, {n_txt} variables de type texte, "
+        f"On observe {n_missing} valeurs manquantes, soit {pct_missing}% de l’ensemble des cellules, "
+        f"réparties sur {n_var_missing} variables. "
+        f"Enfin, {n_const} variables sont constantes."
+    )
+
+    return phrase
